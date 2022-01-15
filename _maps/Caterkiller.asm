97b99d8214d7492117500d574b6a4fddeb1865b3
toastonbread <94253607+toastonbread@users.noreply.github.com>
2022-01-15 14:01:49 +0200

    kill

--- _maps/Caterkiller.asm
+++ _maps/Caterkiller.asm
@@ -1,77 +1,74 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Caterkiller	enemy (MZ, SBZ)
-; ---------------------------------------------------------------------------
-Map_Cat_internal:
-		dc.w byte_16D9E-Map_Cat_internal
-		dc.w byte_16DA4-Map_Cat_internal
-		dc.w byte_16DAA-Map_Cat_internal
-		dc.w byte_16DB0-Map_Cat_internal
-		dc.w byte_16DB6-Map_Cat_internal
-		dc.w byte_16DBC-Map_Cat_internal
-		dc.w byte_16DC2-Map_Cat_internal
-		dc.w byte_16DC8-Map_Cat_internal
-		dc.w byte_16DCE-Map_Cat_internal
-		dc.w byte_16DD4-Map_Cat_internal
-		dc.w byte_16DDA-Map_Cat_internal
-		dc.w byte_16DE0-Map_Cat_internal
-		dc.w byte_16DE6-Map_Cat_internal
-		dc.w byte_16DEC-Map_Cat_internal
-		dc.w byte_16DF2-Map_Cat_internal
-		dc.w byte_16DF8-Map_Cat_internal
-		dc.w byte_16DFE-Map_Cat_internal
-		dc.w byte_16E04-Map_Cat_internal
-		dc.w byte_16E0A-Map_Cat_internal
-		dc.w byte_16E10-Map_Cat_internal
-		dc.w byte_16E16-Map_Cat_internal
-		dc.w byte_16E1C-Map_Cat_internal
-		dc.w byte_16E22-Map_Cat_internal
-		dc.w byte_16E28-Map_Cat_internal
-byte_16D9E:	dc.b 1
-		dc.b $F2, 6, 0,	0, $F8
-byte_16DA4:	dc.b 1
-		dc.b $F1, 6, 0,	0, $F8
-byte_16DAA:	dc.b 1
-		dc.b $F0, 6, 0,	0, $F8
-byte_16DB0:	dc.b 1
-		dc.b $EF, 6, 0,	0, $F8
-byte_16DB6:	dc.b 1
-		dc.b $EE, 6, 0,	0, $F8
-byte_16DBC:	dc.b 1
-		dc.b $ED, 6, 0,	0, $F8
-byte_16DC2:	dc.b 1
-		dc.b $EC, 6, 0,	0, $F8
-byte_16DC8:	dc.b 1
-		dc.b $EB, 6, 0,	0, $F8
-byte_16DCE:	dc.b 1
-		dc.b $F8, 5, 0,	$C, $F8
-byte_16DD4:	dc.b 1
-		dc.b $F7, 5, 0,	$C, $F8
-byte_16DDA:	dc.b 1
-		dc.b $F6, 5, 0,	$C, $F8
-byte_16DE0:	dc.b 1
-		dc.b $F5, 5, 0,	$C, $F8
-byte_16DE6:	dc.b 1
-		dc.b $F4, 5, 0,	$C, $F8
-byte_16DEC:	dc.b 1
-		dc.b $F3, 5, 0,	$C, $F8
-byte_16DF2:	dc.b 1
-		dc.b $F2, 5, 0,	$C, $F8
-byte_16DF8:	dc.b 1
-		dc.b $F1, 5, 0,	$C, $F8
-byte_16DFE:	dc.b 1
-		dc.b $F2, 6, 0,	6, $F8
-byte_16E04:	dc.b 1
-		dc.b $F1, 6, 0,	6, $F8
-byte_16E0A:	dc.b 1
-		dc.b $F0, 6, 0,	6, $F8
-byte_16E10:	dc.b 1
-		dc.b $EF, 6, 0,	6, $F8
-byte_16E16:	dc.b 1
-		dc.b $EE, 6, 0,	6, $F8
-byte_16E1C:	dc.b 1
-		dc.b $ED, 6, 0,	6, $F8
-byte_16E22:	dc.b 1
-		dc.b $EC, 6, 0,	6, $F8
-byte_16E28:	dc.b 1
-		dc.b $EB, 6, 0,	6, $F8
-		even
+Map_219c:
+Map_219c_0: 	dc.w Map_219c_30-Map_219c
+Map_219c_2: 	dc.w Map_219c_36-Map_219c
+Map_219c_4: 	dc.w Map_219c_3C-Map_219c
+Map_219c_6: 	dc.w Map_219c_42-Map_219c
+Map_219c_8: 	dc.w Map_219c_48-Map_219c
+Map_219c_A: 	dc.w Map_219c_4E-Map_219c
+Map_219c_C: 	dc.w Map_219c_54-Map_219c
+Map_219c_E: 	dc.w Map_219c_5A-Map_219c
+Map_219c_10: 	dc.w Map_219c_60-Map_219c
+Map_219c_12: 	dc.w Map_219c_66-Map_219c
+Map_219c_14: 	dc.w Map_219c_6C-Map_219c
+Map_219c_16: 	dc.w Map_219c_72-Map_219c
+Map_219c_18: 	dc.w Map_219c_78-Map_219c
+Map_219c_1A: 	dc.w Map_219c_7E-Map_219c
+Map_219c_1C: 	dc.w Map_219c_84-Map_219c
+Map_219c_1E: 	dc.w Map_219c_8A-Map_219c
+Map_219c_20: 	dc.w Map_219c_90-Map_219c
+Map_219c_22: 	dc.w Map_219c_96-Map_219c
+Map_219c_24: 	dc.w Map_219c_9C-Map_219c
+Map_219c_26: 	dc.w Map_219c_A2-Map_219c
+Map_219c_28: 	dc.w Map_219c_A8-Map_219c
+Map_219c_2A: 	dc.w Map_219c_AE-Map_219c
+Map_219c_2C: 	dc.w Map_219c_B4-Map_219c
+Map_219c_2E: 	dc.w Map_219c_BA-Map_219c
+Map_219c_30: 	dc.b $1
+	dc.b $F2, $6, $0, $0, $F8
+Map_219c_36: 	dc.b $1
+	dc.b $F1, $6, $0, $0, $F8
+Map_219c_3C: 	dc.b $1
+	dc.b $F0, $6, $0, $0, $F8
+Map_219c_42: 	dc.b $1
+	dc.b $EF, $6, $0, $0, $F8
+Map_219c_48: 	dc.b $1
+	dc.b $EE, $6, $0, $0, $F8
+Map_219c_4E: 	dc.b $1
+	dc.b $ED, $6, $0, $0, $F8
+Map_219c_54: 	dc.b $1
+	dc.b $EC, $6, $0, $0, $F8
+Map_219c_5A: 	dc.b $1
+	dc.b $EB, $6, $0, $0, $F8
+Map_219c_60: 	dc.b $1
+	dc.b $F8, $5, $0, $C, $F8
+Map_219c_66: 	dc.b $1
+	dc.b $F7, $5, $0, $C, $F8
+Map_219c_6C: 	dc.b $1
+	dc.b $F6, $5, $0, $C, $F8
+Map_219c_72: 	dc.b $1
+	dc.b $F5, $5, $0, $C, $F8
+Map_219c_78: 	dc.b $1
+	dc.b $F4, $5, $0, $C, $F8
+Map_219c_7E: 	dc.b $1
+	dc.b $F3, $5, $0, $C, $F8
+Map_219c_84: 	dc.b $1
+	dc.b $F2, $5, $0, $C, $F8
+Map_219c_8A: 	dc.b $1
+	dc.b $F1, $5, $0, $C, $F8
+Map_219c_90: 	dc.b $1
+	dc.b $F2, $6, $0, $6, $F8
+Map_219c_96: 	dc.b $1
+	dc.b $F1, $6, $0, $6, $F8
+Map_219c_9C: 	dc.b $1
+	dc.b $F0, $6, $0, $6, $F8
+Map_219c_A2: 	dc.b $1
+	dc.b $EF, $6, $0, $6, $F8
+Map_219c_A8: 	dc.b $1
+	dc.b $EE, $6, $0, $6, $F8
+Map_219c_AE: 	dc.b $1
+	dc.b $ED, $6, $0, $6, $F8
+Map_219c_B4: 	dc.b $1
+	dc.b $EC, $6, $0, $6, $F8
+Map_219c_BA: 	dc.b $1
+	dc.b $EB, $6, $0, $6, $F8
+	even
