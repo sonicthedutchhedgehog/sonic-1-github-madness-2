d0c04a9d3e8315395d23540a7899d533f9f899ca
toastonbread <94253607+toastonbread@users.noreply.github.com>
2022-01-15 14:34:06 +0200

    furry monitor

--- _maps/Monitor.asm
+++ _maps/Monitor.asm
@@ -1,48 +1,48 @@
-Map_718c:
-Map_718c_0: 	dc.w Map_718c_18-Map_718c
-Map_718c_2: 	dc.w Map_718c_1E-Map_718c
-Map_718c_4: 	dc.w Map_718c_29-Map_718c
-Map_718c_6: 	dc.w Map_718c_34-Map_718c
-Map_718c_8: 	dc.w Map_718c_3F-Map_718c
-Map_718c_A: 	dc.w Map_718c_4A-Map_718c
-Map_718c_C: 	dc.w Map_718c_55-Map_718c
-Map_718c_E: 	dc.w Map_718c_60-Map_718c
-Map_718c_10: 	dc.w Map_718c_6B-Map_718c
-Map_718c_12: 	dc.w Map_718c_76-Map_718c
-Map_718c_14: 	dc.w Map_718c_81-Map_718c
-Map_718c_16: 	dc.w Map_718c_8C-Map_718c
-Map_718c_18: 	dc.b $1
+Map_978b:
+Map_978b_0: 	dc.w Map_978b_18-Map_978b
+Map_978b_2: 	dc.w Map_978b_1E-Map_978b
+Map_978b_4: 	dc.w Map_978b_29-Map_978b
+Map_978b_6: 	dc.w Map_978b_34-Map_978b
+Map_978b_8: 	dc.w Map_978b_3F-Map_978b
+Map_978b_A: 	dc.w Map_978b_4A-Map_978b
+Map_978b_C: 	dc.w Map_978b_55-Map_978b
+Map_978b_E: 	dc.w Map_978b_60-Map_978b
+Map_978b_10: 	dc.w Map_978b_6B-Map_978b
+Map_978b_12: 	dc.w Map_978b_76-Map_978b
+Map_978b_14: 	dc.w Map_978b_81-Map_978b
+Map_978b_16: 	dc.w Map_978b_8C-Map_978b
+Map_978b_18: 	dc.b $1
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_1E: 	dc.b $2
+Map_978b_1E: 	dc.b $2
 	dc.b $F5, $5, $0, $10, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_29: 	dc.b $2
+Map_978b_29: 	dc.b $2
 	dc.b $F5, $5, $0, $14, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_34: 	dc.b $2
+Map_978b_34: 	dc.b $2
 	dc.b $F5, $5, $0, $18, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_3F: 	dc.b $2
+Map_978b_3F: 	dc.b $2
 	dc.b $F5, $5, $0, $1C, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_4A: 	dc.b $2
+Map_978b_4A: 	dc.b $2
 	dc.b $F5, $5, $0, $24, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_55: 	dc.b $2
+Map_978b_55: 	dc.b $2
 	dc.b $F5, $5, $0, $28, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_60: 	dc.b $2
+Map_978b_60: 	dc.b $2
 	dc.b $F5, $5, $0, $2C, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_6B: 	dc.b $2
+Map_978b_6B: 	dc.b $2
 	dc.b $F5, $5, $0, $30, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_76: 	dc.b $2
+Map_978b_76: 	dc.b $2
 	dc.b $F5, $5, $0, $34, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_81: 	dc.b $2
+Map_978b_81: 	dc.b $2
 	dc.b $F5, $5, $0, $20, $F8
 	dc.b $EF, $F, $0, $0, $F0
-Map_718c_8C: 	dc.b $1
+Map_978b_8C: 	dc.b $1
 	dc.b $FF, $D, $0, $38, $F0
 	even
