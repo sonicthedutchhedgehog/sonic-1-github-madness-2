5411f651e9f5c09a74b1e24cdadeedfc4504561c
toastonbread <94253607+toastonbread@users.noreply.github.com>
2022-01-16 17:53:23 +0200

    mn

--- _maps/Harpoon.asm
+++ _maps/Harpoon.asm
@@ -1,25 +1,22 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - harpoon (LZ)
-; ---------------------------------------------------------------------------
-Map_Harp_internal:
-		dc.w .h_retracted-Map_Harp_internal
-		dc.w .h_middle-Map_Harp_internal
-		dc.w .h_extended-Map_Harp_internal
-		dc.w .v_retracted-Map_Harp_internal
-		dc.w .v_middle-Map_Harp_internal
-		dc.w .v_extended-Map_Harp_internal
-.h_retracted:	dc.b 1
-		dc.b $FC, 4, 0,	0, $F8
-.h_middle:	dc.b 1
-		dc.b $FC, $C, 0, 2, $F8
-.h_extended:	dc.b 2
-		dc.b $FC, 8, 0,	6, $F8
-		dc.b $FC, 8, 0,	3, $10
-.v_retracted:	dc.b 1
-		dc.b $F8, 1, 0,	9, $FC
-.v_middle:	dc.b 1
-		dc.b $E8, 3, 0,	$B, $FC
-.v_extended:	dc.b 2
-		dc.b $D8, 2, 0,	$B, $FC
-		dc.b $F0, 2, 0,	$F, $FC
-		even
+Map_441f:
+Map_441f_0: 	dc.w Map_441f_C-Map_441f
+Map_441f_2: 	dc.w Map_441f_12-Map_441f
+Map_441f_4: 	dc.w Map_441f_18-Map_441f
+Map_441f_6: 	dc.w Map_441f_23-Map_441f
+Map_441f_8: 	dc.w Map_441f_29-Map_441f
+Map_441f_A: 	dc.w Map_441f_2F-Map_441f
+Map_441f_C: 	dc.b $1
+	dc.b $FC, $4, $0, $0, $F8
+Map_441f_12: 	dc.b $1
+	dc.b $FC, $C, $0, $2, $F8
+Map_441f_18: 	dc.b $2
+	dc.b $FC, $8, $0, $6, $F8
+	dc.b $FC, $8, $0, $3, $10
+Map_441f_23: 	dc.b $1
+	dc.b $F8, $1, $0, $9, $FC
+Map_441f_29: 	dc.b $1
+	dc.b $E8, $3, $0, $B, $FC
+Map_441f_2F: 	dc.b $2
+	dc.b $D8, $2, $0, $B, $FC
+	dc.b $F0, $2, $0, $F, $FC
+	even
--- _maps/Springs.asm
+++ _maps/Springs.asm
@@ -1,24 +1,24 @@
-Map_d988:
-Map_d988_0: 	dc.w Map_d988_C-Map_d988
-Map_d988_2: 	dc.w Map_d988_17-Map_d988
-Map_d988_4: 	dc.w Map_d988_1D-Map_d988
-Map_d988_6: 	dc.w Map_d988_2D-Map_d988
-Map_d988_8: 	dc.w Map_d988_33-Map_d988
-Map_d988_A: 	dc.w Map_d988_39-Map_d988
-Map_d988_C: 	dc.b $2
+Map_7e44:
+Map_7e44_0: 	dc.w Map_7e44_C-Map_7e44
+Map_7e44_2: 	dc.w Map_7e44_17-Map_7e44
+Map_7e44_4: 	dc.w Map_7e44_1D-Map_7e44
+Map_7e44_6: 	dc.w Map_7e44_2D-Map_7e44
+Map_7e44_8: 	dc.w Map_7e44_33-Map_7e44
+Map_7e44_A: 	dc.w Map_7e44_39-Map_7e44
+Map_7e44_C: 	dc.b $2
 	dc.b $F8, $C, $0, $0, $F0
 	dc.b $0, $C, $0, $4, $F0
-Map_d988_17: 	dc.b $1
-	dc.b $0, $C, $0, $0, $F0
-Map_d988_1D: 	dc.b $3
-	dc.b $E8, $C, $0, $0, $F0
-	dc.b $F0, $5, $0, $8, $F8
-	dc.b $0, $C, $0, $C, $F0
-Map_d988_2D: 	dc.b $1
+Map_7e44_17: 	dc.b $1
+	dc.b $0, $C, $0, $10, $F2
+Map_7e44_1D: 	dc.b $3
+	dc.b $F0, $C, $0, $14, $F0
+	dc.b $8, $C, $0, $18, $F0
+	dc.b $F8, $5, $0, $1C, $F8
+Map_7e44_2D: 	dc.b $1
 	dc.b $F0, $7, $0, $0, $F8
-Map_d988_33: 	dc.b $1
+Map_7e44_33: 	dc.b $1
 	dc.b $F0, $3, $0, $4, $F8
-Map_d988_39: 	dc.b $4
+Map_7e44_39: 	dc.b $4
 	dc.b $F0, $3, $0, $4, $10
 	dc.b $F8, $9, $0, $8, $F8
 	dc.b $F0, $0, $0, $0, $F8
