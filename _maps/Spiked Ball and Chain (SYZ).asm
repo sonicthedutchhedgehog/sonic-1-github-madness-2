0588da95879fbe7854705d2711d112ec4a693c73
toastonbread <94253607+toastonbread@users.noreply.github.com>
2022-01-16 18:48:01 +0200

    nbfrmrf

--- _maps/Spiked Ball and Chain (SYZ).asm
+++ _maps/Spiked Ball and Chain (SYZ).asm
@@ -1,8 +1,5 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - chain of spiked balls (SYZ)
-; ---------------------------------------------------------------------------
-Map_SBall_internal:
-		dc.w .f0-Map_SBall_internal
-.f0:		dc.b 1
-		dc.b $F8, 5, 0,	0, $F8
-		even
+Map_1fef:
+Map_1fef_0: 	dc.w Map_1fef_2-Map_1fef
+Map_1fef_2: 	dc.b $1
+	dc.b $F8, $5, $0, $0, $F8
+	even
