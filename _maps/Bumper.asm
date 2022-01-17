47e268f93dd2e28655409bededc472ff118f5038
toastonbread <94253607+toastonbread@users.noreply.github.com>
2022-01-17 11:12:46 -0800

    heko

--- _maps/Bumper.asm
+++ _maps/Bumper.asm
@@ -1,17 +1,14 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - pinball bumper (SYZ)
-; ---------------------------------------------------------------------------
-Map_Bump_internal:
-		dc.w .normal-Map_Bump_internal
-		dc.w .bumped1-Map_Bump_internal
-		dc.w .bumped2-Map_Bump_internal
-.normal:	dc.b 2
-		dc.b $F0, 7, 0,	0, $F0
-		dc.b $F0, 7, 8,	0, 0
-.bumped1:	dc.b 2
-		dc.b $F4, 6, 0,	8, $F4
-		dc.b $F4, 2, 8,	8, 4
-.bumped2:	dc.b 2
-		dc.b $F0, 7, 0,	$E, $F0
-		dc.b $F0, 7, 8,	$E, 0
-		even
+Map_c4d1:
+Map_c4d1_0: 	dc.w Map_c4d1_6-Map_c4d1
+Map_c4d1_2: 	dc.w Map_c4d1_11-Map_c4d1
+Map_c4d1_4: 	dc.w Map_c4d1_1C-Map_c4d1
+Map_c4d1_6: 	dc.b $2
+	dc.b $F0, $7, $0, $0, $F0
+	dc.b $F0, $7, $8, $0, $0
+Map_c4d1_11: 	dc.b $2
+	dc.b $F4, $6, $0, $8, $F4
+	dc.b $F4, $2, $8, $8, $4
+Map_c4d1_1C: 	dc.b $2
+	dc.b $F0, $7, $0, $E, $F0
+	dc.b $F0, $7, $8, $E, $0
+	even
--- _maps/Fan.asm
+++ _maps/Fan.asm
@@ -1,19 +1,22 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - fans (SLZ)
-; ---------------------------------------------------------------------------
-Map_Fan_internal:
-		dc.w .fan1-Map_Fan_internal
-		dc.w .fan2-Map_Fan_internal
-		dc.w .fan3-Map_Fan_internal
-		dc.w .fan2-Map_Fan_internal
-		dc.w .fan1-Map_Fan_internal
-.fan1:		dc.b 2
-		dc.b $F0, 9, 0,	0, $F8
-		dc.b 0,	$D, 0, 6, $F0
-.fan2:		dc.b 2
-		dc.b $F0, $D, 0, $E, $F0
-		dc.b 0,	$D, 0, $16, $F0
-.fan3:		dc.b 2
-		dc.b $F0, $D, 0, $1E, $F0
-		dc.b 0,	9, 0, $26, $F8
-		even
+Map_d1ab:
+Map_d1ab_0: 	dc.w Map_d1ab_A-Map_d1ab
+Map_d1ab_2: 	dc.w Map_d1ab_15-Map_d1ab
+Map_d1ab_4: 	dc.w Map_d1ab_20-Map_d1ab
+Map_d1ab_6: 	dc.w Map_d1ab_2B-Map_d1ab
+Map_d1ab_8: 	dc.w Map_d1ab_36-Map_d1ab
+Map_d1ab_A: 	dc.b $2
+	dc.b $F0, $9, $0, $0, $F8
+	dc.b $0, $D, $0, $6, $F0
+Map_d1ab_15: 	dc.b $2
+	dc.b $F0, $D, $0, $E, $F0
+	dc.b $0, $D, $0, $16, $F0
+Map_d1ab_20: 	dc.b $2
+	dc.b $F0, $D, $0, $1E, $F0
+	dc.b $0, $9, $0, $26, $F8
+Map_d1ab_2B: 	dc.b $2
+	dc.b $F0, $D, $0, $E, $F0
+	dc.b $0, $D, $0, $16, $F0
+Map_d1ab_36: 	dc.b $2
+	dc.b $F0, $9, $0, $0, $F8
+	dc.b $0, $D, $0, $6, $F0
+	even
