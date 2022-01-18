38caf5161855043d43037372bac4c91ad7de8045
GuesterReal <96834208+GuesterReal@users.noreply.github.com>
2022-01-18 21:35:17 +0300

    barigadam dam dam dam barigadam dam dam barigadam dam dam barigdam dam dam dam barigadam dam dam barigadam dam dam

--- _maps/Sonic.asm
+++ _maps/Sonic.asm
@@ -1,438 +1,438 @@
-Map_6161:
-Map_6161_0: 	dc.w Map_6161_B2-Map_6161
-Map_6161_2: 	dc.w Map_6161_B3-Map_6161
-Map_6161_4: 	dc.w Map_6161_BE-Map_6161
-Map_6161_6: 	dc.w Map_6161_CE-Map_6161
-Map_6161_8: 	dc.w Map_6161_DE-Map_6161
-Map_6161_A: 	dc.w Map_6161_EE-Map_6161
-Map_6161_C: 	dc.w Map_6161_FE-Map_6161
-Map_6161_E: 	dc.w Map_6161_113-Map_6161
-Map_6161_10: 	dc.w Map_6161_11E-Map_6161
-Map_6161_12: 	dc.w Map_6161_129-Map_6161
-Map_6161_14: 	dc.w Map_6161_13E-Map_6161
-Map_6161_16: 	dc.w Map_6161_149-Map_6161
-Map_6161_18: 	dc.w Map_6161_159-Map_6161
-Map_6161_1A: 	dc.w Map_6161_173-Map_6161
-Map_6161_1C: 	dc.w Map_6161_192-Map_6161
-Map_6161_1E: 	dc.w Map_6161_1A7-Map_6161
-Map_6161_20: 	dc.w Map_6161_1C1-Map_6161
-Map_6161_22: 	dc.w Map_6161_1D6-Map_6161
-Map_6161_24: 	dc.w Map_6161_1F0-Map_6161
-Map_6161_26: 	dc.w Map_6161_205-Map_6161
-Map_6161_28: 	dc.w Map_6161_210-Map_6161
-Map_6161_2A: 	dc.w Map_6161_21B-Map_6161
-Map_6161_2C: 	dc.w Map_6161_230-Map_6161
-Map_6161_2E: 	dc.w Map_6161_23B-Map_6161
-Map_6161_30: 	dc.w Map_6161_24B-Map_6161
-Map_6161_32: 	dc.w Map_6161_26A-Map_6161
-Map_6161_34: 	dc.w Map_6161_289-Map_6161
-Map_6161_36: 	dc.w Map_6161_29E-Map_6161
-Map_6161_38: 	dc.w Map_6161_2B8-Map_6161
-Map_6161_3A: 	dc.w Map_6161_2CD-Map_6161
-Map_6161_3C: 	dc.w Map_6161_2E7-Map_6161
-Map_6161_3E: 	dc.w Map_6161_2F2-Map_6161
-Map_6161_40: 	dc.w Map_6161_2FD-Map_6161
-Map_6161_42: 	dc.w Map_6161_308-Map_6161
-Map_6161_44: 	dc.w Map_6161_313-Map_6161
-Map_6161_46: 	dc.w Map_6161_328-Map_6161
-Map_6161_48: 	dc.w Map_6161_338-Map_6161
-Map_6161_4A: 	dc.w Map_6161_34D-Map_6161
-Map_6161_4C: 	dc.w Map_6161_35D-Map_6161
-Map_6161_4E: 	dc.w Map_6161_368-Map_6161
-Map_6161_50: 	dc.w Map_6161_373-Map_6161
-Map_6161_52: 	dc.w Map_6161_37E-Map_6161
-Map_6161_54: 	dc.w Map_6161_389-Map_6161
-Map_6161_56: 	dc.w Map_6161_39E-Map_6161
-Map_6161_58: 	dc.w Map_6161_3A9-Map_6161
-Map_6161_5A: 	dc.w Map_6161_3BE-Map_6161
-Map_6161_5C: 	dc.w Map_6161_3C9-Map_6161
-Map_6161_5E: 	dc.w Map_6161_3CF-Map_6161
-Map_6161_60: 	dc.w Map_6161_3D5-Map_6161
-Map_6161_62: 	dc.w Map_6161_3DB-Map_6161
-Map_6161_64: 	dc.w Map_6161_3E1-Map_6161
-Map_6161_66: 	dc.w Map_6161_3E7-Map_6161
-Map_6161_68: 	dc.w Map_6161_3F2-Map_6161
-Map_6161_6A: 	dc.w Map_6161_3F8-Map_6161
-Map_6161_6C: 	dc.w Map_6161_403-Map_6161
-Map_6161_6E: 	dc.w Map_6161_409-Map_6161
-Map_6161_70: 	dc.w Map_6161_414-Map_6161
-Map_6161_72: 	dc.w Map_6161_41F-Map_6161
-Map_6161_74: 	dc.w Map_6161_434-Map_6161
-Map_6161_76: 	dc.w Map_6161_444-Map_6161
-Map_6161_78: 	dc.w Map_6161_454-Map_6161
-Map_6161_7A: 	dc.w Map_6161_464-Map_6161
-Map_6161_7C: 	dc.w Map_6161_46F-Map_6161
-Map_6161_7E: 	dc.w Map_6161_47F-Map_6161
-Map_6161_80: 	dc.w Map_6161_48F-Map_6161
-Map_6161_82: 	dc.w Map_6161_49F-Map_6161
-Map_6161_84: 	dc.w Map_6161_4B4-Map_6161
-Map_6161_86: 	dc.w Map_6161_4C9-Map_6161
-Map_6161_88: 	dc.w Map_6161_4E3-Map_6161
-Map_6161_8A: 	dc.w Map_6161_4FD-Map_6161
-Map_6161_8C: 	dc.w Map_6161_508-Map_6161
-Map_6161_8E: 	dc.w Map_6161_518-Map_6161
-Map_6161_90: 	dc.w Map_6161_523-Map_6161
-Map_6161_92: 	dc.w Map_6161_533-Map_6161
-Map_6161_94: 	dc.w Map_6161_53E-Map_6161
-Map_6161_96: 	dc.w Map_6161_54E-Map_6161
-Map_6161_98: 	dc.w Map_6161_55E-Map_6161
-Map_6161_9A: 	dc.w Map_6161_578-Map_6161
-Map_6161_9C: 	dc.w Map_6161_592-Map_6161
-Map_6161_9E: 	dc.w Map_6161_59D-Map_6161
-Map_6161_A0: 	dc.w Map_6161_5AD-Map_6161
-Map_6161_A2: 	dc.w Map_6161_5B3-Map_6161
-Map_6161_A4: 	dc.w Map_6161_5B9-Map_6161
-Map_6161_A6: 	dc.w Map_6161_5BF-Map_6161
-Map_6161_A8: 	dc.w Map_6161_5CF-Map_6161
-Map_6161_AA: 	dc.w Map_6161_5DF-Map_6161
-Map_6161_AC: 	dc.w Map_6161_5EF-Map_6161
-Map_6161_AE: 	dc.w Map_6161_5FF-Map_6161
-Map_6161_B0: 	dc.w Map_6161_60A-Map_6161
-Map_6161_B2: 	dc.b $0
-Map_6161_B3: 	dc.b $2
+Map_fc31:
+Map_fc31_0: 	dc.w Map_fc31_B2-Map_fc31
+Map_fc31_2: 	dc.w Map_fc31_B3-Map_fc31
+Map_fc31_4: 	dc.w Map_fc31_BE-Map_fc31
+Map_fc31_6: 	dc.w Map_fc31_CE-Map_fc31
+Map_fc31_8: 	dc.w Map_fc31_DE-Map_fc31
+Map_fc31_A: 	dc.w Map_fc31_EE-Map_fc31
+Map_fc31_C: 	dc.w Map_fc31_FE-Map_fc31
+Map_fc31_E: 	dc.w Map_fc31_113-Map_fc31
+Map_fc31_10: 	dc.w Map_fc31_11E-Map_fc31
+Map_fc31_12: 	dc.w Map_fc31_129-Map_fc31
+Map_fc31_14: 	dc.w Map_fc31_13E-Map_fc31
+Map_fc31_16: 	dc.w Map_fc31_149-Map_fc31
+Map_fc31_18: 	dc.w Map_fc31_159-Map_fc31
+Map_fc31_1A: 	dc.w Map_fc31_173-Map_fc31
+Map_fc31_1C: 	dc.w Map_fc31_192-Map_fc31
+Map_fc31_1E: 	dc.w Map_fc31_1A7-Map_fc31
+Map_fc31_20: 	dc.w Map_fc31_1C1-Map_fc31
+Map_fc31_22: 	dc.w Map_fc31_1D6-Map_fc31
+Map_fc31_24: 	dc.w Map_fc31_1F0-Map_fc31
+Map_fc31_26: 	dc.w Map_fc31_205-Map_fc31
+Map_fc31_28: 	dc.w Map_fc31_210-Map_fc31
+Map_fc31_2A: 	dc.w Map_fc31_21B-Map_fc31
+Map_fc31_2C: 	dc.w Map_fc31_230-Map_fc31
+Map_fc31_2E: 	dc.w Map_fc31_23B-Map_fc31
+Map_fc31_30: 	dc.w Map_fc31_24B-Map_fc31
+Map_fc31_32: 	dc.w Map_fc31_26A-Map_fc31
+Map_fc31_34: 	dc.w Map_fc31_289-Map_fc31
+Map_fc31_36: 	dc.w Map_fc31_29E-Map_fc31
+Map_fc31_38: 	dc.w Map_fc31_2B8-Map_fc31
+Map_fc31_3A: 	dc.w Map_fc31_2CD-Map_fc31
+Map_fc31_3C: 	dc.w Map_fc31_2E7-Map_fc31
+Map_fc31_3E: 	dc.w Map_fc31_2F2-Map_fc31
+Map_fc31_40: 	dc.w Map_fc31_2FD-Map_fc31
+Map_fc31_42: 	dc.w Map_fc31_308-Map_fc31
+Map_fc31_44: 	dc.w Map_fc31_313-Map_fc31
+Map_fc31_46: 	dc.w Map_fc31_328-Map_fc31
+Map_fc31_48: 	dc.w Map_fc31_338-Map_fc31
+Map_fc31_4A: 	dc.w Map_fc31_34D-Map_fc31
+Map_fc31_4C: 	dc.w Map_fc31_35D-Map_fc31
+Map_fc31_4E: 	dc.w Map_fc31_368-Map_fc31
+Map_fc31_50: 	dc.w Map_fc31_373-Map_fc31
+Map_fc31_52: 	dc.w Map_fc31_37E-Map_fc31
+Map_fc31_54: 	dc.w Map_fc31_389-Map_fc31
+Map_fc31_56: 	dc.w Map_fc31_39E-Map_fc31
+Map_fc31_58: 	dc.w Map_fc31_3A9-Map_fc31
+Map_fc31_5A: 	dc.w Map_fc31_3BE-Map_fc31
+Map_fc31_5C: 	dc.w Map_fc31_3C9-Map_fc31
+Map_fc31_5E: 	dc.w Map_fc31_3CF-Map_fc31
+Map_fc31_60: 	dc.w Map_fc31_3D5-Map_fc31
+Map_fc31_62: 	dc.w Map_fc31_3DB-Map_fc31
+Map_fc31_64: 	dc.w Map_fc31_3E1-Map_fc31
+Map_fc31_66: 	dc.w Map_fc31_3E7-Map_fc31
+Map_fc31_68: 	dc.w Map_fc31_3F2-Map_fc31
+Map_fc31_6A: 	dc.w Map_fc31_3F8-Map_fc31
+Map_fc31_6C: 	dc.w Map_fc31_403-Map_fc31
+Map_fc31_6E: 	dc.w Map_fc31_409-Map_fc31
+Map_fc31_70: 	dc.w Map_fc31_414-Map_fc31
+Map_fc31_72: 	dc.w Map_fc31_41F-Map_fc31
+Map_fc31_74: 	dc.w Map_fc31_434-Map_fc31
+Map_fc31_76: 	dc.w Map_fc31_444-Map_fc31
+Map_fc31_78: 	dc.w Map_fc31_454-Map_fc31
+Map_fc31_7A: 	dc.w Map_fc31_464-Map_fc31
+Map_fc31_7C: 	dc.w Map_fc31_46F-Map_fc31
+Map_fc31_7E: 	dc.w Map_fc31_47F-Map_fc31
+Map_fc31_80: 	dc.w Map_fc31_48F-Map_fc31
+Map_fc31_82: 	dc.w Map_fc31_49F-Map_fc31
+Map_fc31_84: 	dc.w Map_fc31_4B4-Map_fc31
+Map_fc31_86: 	dc.w Map_fc31_4C9-Map_fc31
+Map_fc31_88: 	dc.w Map_fc31_4E3-Map_fc31
+Map_fc31_8A: 	dc.w Map_fc31_4FD-Map_fc31
+Map_fc31_8C: 	dc.w Map_fc31_508-Map_fc31
+Map_fc31_8E: 	dc.w Map_fc31_518-Map_fc31
+Map_fc31_90: 	dc.w Map_fc31_523-Map_fc31
+Map_fc31_92: 	dc.w Map_fc31_533-Map_fc31
+Map_fc31_94: 	dc.w Map_fc31_53E-Map_fc31
+Map_fc31_96: 	dc.w Map_fc31_54E-Map_fc31
+Map_fc31_98: 	dc.w Map_fc31_55E-Map_fc31
+Map_fc31_9A: 	dc.w Map_fc31_578-Map_fc31
+Map_fc31_9C: 	dc.w Map_fc31_592-Map_fc31
+Map_fc31_9E: 	dc.w Map_fc31_59D-Map_fc31
+Map_fc31_A0: 	dc.w Map_fc31_5AD-Map_fc31
+Map_fc31_A2: 	dc.w Map_fc31_5B3-Map_fc31
+Map_fc31_A4: 	dc.w Map_fc31_5B9-Map_fc31
+Map_fc31_A6: 	dc.w Map_fc31_5BF-Map_fc31
+Map_fc31_A8: 	dc.w Map_fc31_5CF-Map_fc31
+Map_fc31_AA: 	dc.w Map_fc31_5DF-Map_fc31
+Map_fc31_AC: 	dc.w Map_fc31_5EF-Map_fc31
+Map_fc31_AE: 	dc.w Map_fc31_5FF-Map_fc31
+Map_fc31_B0: 	dc.w Map_fc31_60A-Map_fc31
+Map_fc31_B2: 	dc.b $0
+Map_fc31_B3: 	dc.b $2
 	dc.b $ED, $B, $0, $0, $F4
 	dc.b $D, $8, $0, $C, $F4
-Map_6161_BE: 	dc.b $3
+Map_fc31_BE: 	dc.b $3
 	dc.b $EC, $9, $0, $0, $F0
 	dc.b $FC, $9, $0, $6, $F0
 	dc.b $C, $8, $0, $C, $F8
-Map_6161_CE: 	dc.b $3
+Map_fc31_CE: 	dc.b $3
 	dc.b $EC, $9, $0, $0, $F0
 	dc.b $FC, $9, $0, $6, $F0
 	dc.b $C, $8, $0, $C, $F8
-Map_6161_DE: 	dc.b $3
+Map_fc31_DE: 	dc.b $3
 	dc.b $EC, $9, $0, $0, $F0
 	dc.b $FC, $9, $0, $6, $F0
 	dc.b $C, $8, $0, $C, $F8
-Map_6161_EE: 	dc.b $3
+Map_fc31_EE: 	dc.b $3
 	dc.b $EC, $A, $0, $0, $F0
 	dc.b $4, $8, $0, $9, $F0
 	dc.b $C, $8, $0, $C, $F8
-Map_6161_FE: 	dc.b $4
+Map_fc31_FE: 	dc.b $4
 	dc.b $EB, $D, $0, $0, $EC
 	dc.b $FB, $9, $0, $8, $EC
 	dc.b $FB, $6, $0, $E, $4
 	dc.b $B, $4, $0, $14, $EC
-Map_6161_113: 	dc.b $2
+Map_fc31_113: 	dc.b $2
 	dc.b $EC, $D, $0, $0, $ED
 	dc.b $FC, $E, $0, $8, $F5
-Map_6161_11E: 	dc.b $2
+Map_fc31_11E: 	dc.b $2
 	dc.b $ED, $9, $0, $0, $F3
 	dc.b $FD, $A, $0, $6, $F3
-Map_6161_129: 	dc.b $4
+Map_fc31_129: 	dc.b $4
 	dc.b $EB, $9, $0, $0, $F4
 	dc.b $FB, $9, $0, $6, $EC
 	dc.b $FB, $6, $0, $C, $4
 	dc.b $B, $4, $0, $12, $EC
-Map_6161_13E: 	dc.b $2
+Map_fc31_13E: 	dc.b $2
 	dc.b $EC, $9, $0, $0, $F3
 	dc.b $FC, $E, $0, $6, $EB
-Map_6161_149: 	dc.b $3
+Map_fc31_149: 	dc.b $3
 	dc.b $ED, $D, $0, $0, $EC
 	dc.b $FD, $C, $0, $8, $F4
 	dc.b $5, $9, $0, $C, $F4
-Map_6161_159: 	dc.b $5
+Map_fc31_159: 	dc.b $5
 	dc.b $EB, $9, $0, $0, $EB
 	dc.b $EB, $6, $0, $6, $3
 	dc.b $FB, $8, $0, $C, $EB
 	dc.b $3, $9, $0, $F, $F3
 	dc.b $13, $0, $0, $15, $FB
-Map_6161_173: 	dc.b $6
+Map_fc31_173: 	dc.b $6
 	dc.b $EC, $9, $0, $0, $EC
 	dc.b $EC, $1, $0, $6, $4
 	dc.b $FC, $C, $0, $8, $EC
 	dc.b $4, $9, $0, $C, $F4
 	dc.b $FC, $5, $0, $12, $C
 	dc.b $F4, $0, $0, $16, $14
-Map_6161_192: 	dc.b $4
+Map_fc31_192: 	dc.b $4
 	dc.b $ED, $9, $0, $0, $ED
 	dc.b $ED, $1, $0, $6, $5
 	dc.b $FD, $D, $0, $8, $F5
 	dc.b $D, $8, $0, $10, $FD
-Map_6161_1A7: 	dc.b $5
+Map_fc31_1A7: 	dc.b $5
 	dc.b $EB, $9, $0, $0, $EB
 	dc.b $EB, $5, $0, $6, $3
 	dc.b $FB, $D, $0, $A, $F3
 	dc.b $B, $8, $0, $12, $F3
 	dc.b $13, $4, $0, $15, $FB
-Map_6161_1C1: 	dc.b $4
+Map_fc31_1C1: 	dc.b $4
 	dc.b $EC, $9, $0, $0, $EC
 	dc.b $EC, $1, $0, $6, $4
 	dc.b $FC, $D, $0, $8, $F4
 	dc.b $C, $8, $0, $10, $FC
-Map_6161_1D6: 	dc.b $5
+Map_fc31_1D6: 	dc.b $5
 	dc.b $ED, $9, $0, $0, $ED
 	dc.b $ED, $1, $0, $6, $5
 	dc.b $FD, $0, $0, $8, $ED
 	dc.b $FD, $D, $0, $9, $F5
 	dc.b $D, $8, $0, $11, $FD
-Map_6161_1F0: 	dc.b $4
+Map_fc31_1F0: 	dc.b $4
 	dc.b $F4, $7, $0, $0, $EB
 	dc.b $EC, $9, $0, $8, $FB
 	dc.b $FC, $4, $0, $E, $FB
 	dc.b $4, $9, $0, $10, $FB
-Map_6161_205: 	dc.b $2
+Map_fc31_205: 	dc.b $2
 	dc.b $F4, $7, $0, $0, $EC
 	dc.b $EC, $B, $0, $8, $FC
-Map_6161_210: 	dc.b $2
+Map_fc31_210: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $ED
 	dc.b $F4, $A, $0, $6, $FD
-Map_6161_21B: 	dc.b $4
+Map_fc31_21B: 	dc.b $4
 	dc.b $F4, $6, $0, $0, $EB
 	dc.b $EC, $9, $0, $6, $FB
 	dc.b $FC, $4, $0, $C, $FB
 	dc.b $4, $9, $0, $E, $FB
-Map_6161_230: 	dc.b $2
+Map_fc31_230: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EC
 	dc.b $F4, $B, $0, $6, $FC
-Map_6161_23B: 	dc.b $3
+Map_fc31_23B: 	dc.b $3
 	dc.b $F4, $7, $0, $0, $ED
 	dc.b $EC, $0, $0, $8, $FD
 	dc.b $F4, $A, $0, $9, $FD
-Map_6161_24B: 	dc.b $6
+Map_fc31_24B: 	dc.b $6
 	dc.b $FD, $6, $0, $0, $EB
 	dc.b $ED, $4, $0, $6, $F3
 	dc.b $F5, $4, $0, $8, $EB
 	dc.b $F5, $A, $0, $A, $FB
 	dc.b $D, $0, $0, $13, $FB
 	dc.b $FD, $0, $0, $14, $13
-Map_6161_26A: 	dc.b $6
+Map_fc31_26A: 	dc.b $6
 	dc.b $FC, $6, $0, $0, $EC
 	dc.b $E4, $8, $0, $6, $F4
 	dc.b $EC, $4, $0, $9, $FC
 	dc.b $F4, $4, $0, $B, $EC
 	dc.b $F4, $A, $0, $D, $FC
 	dc.b $C, $0, $0, $16, $FC
-Map_6161_289: 	dc.b $4
+Map_fc31_289: 	dc.b $4
 	dc.b $FB, $6, $0, $0, $ED
 	dc.b $F3, $4, $0, $6, $ED
 	dc.b $EB, $A, $0, $8, $FD
 	dc.b $3, $4, $0, $11, $FD
-Map_6161_29E: 	dc.b $5
+Map_fc31_29E: 	dc.b $5
 	dc.b $FD, $6, $0, $0, $EB
 	dc.b $ED, $8, $0, $6, $F3
 	dc.b $F5, $4, $0, $9, $EB
 	dc.b $F5, $D, $0, $B, $FB
 	dc.b $5, $8, $0, $13, $FB
-Map_6161_2B8: 	dc.b $4
+Map_fc31_2B8: 	dc.b $4
 	dc.b $FC, $6, $0, $0, $EC
 	dc.b $F4, $4, $0, $6, $EC
 	dc.b $EC, $A, $0, $8, $FC
 	dc.b $4, $4, $0, $11, $FC
-Map_6161_2CD: 	dc.b $5
+Map_fc31_2CD: 	dc.b $5
 	dc.b $FB, $6, $0, $0, $ED
 	dc.b $EB, $A, $0, $6, $FD
 	dc.b $F3, $4, $0, $F, $ED
 	dc.b $3, $4, $0, $11, $FD
 	dc.b $B, $0, $0, $13, $FD
-Map_6161_2E7: 	dc.b $2
+Map_fc31_2E7: 	dc.b $2
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
-Map_6161_2F2: 	dc.b $2
+Map_fc31_2F2: 	dc.b $2
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
-Map_6161_2FD: 	dc.b $2
+Map_fc31_2FD: 	dc.b $2
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
-Map_6161_308: 	dc.b $2
+Map_fc31_308: 	dc.b $2
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
-Map_6161_313: 	dc.b $4
+Map_fc31_313: 	dc.b $4
 	dc.b $EE, $9, $0, $0, $EE
 	dc.b $EE, $1, $0, $6, $6
 	dc.b $FE, $E, $0, $8, $F6
 	dc.b $FE, $0, $0, $14, $EE
-Map_6161_328: 	dc.b $3
+Map_fc31_328: 	dc.b $3
 	dc.b $EE, $9, $0, $0, $EE
 	dc.b $EE, $1, $0, $6, $6
 	dc.b $FE, $E, $0, $8, $F6
-Map_6161_338: 	dc.b $4
+Map_fc31_338: 	dc.b $4
 	dc.b $EE, $9, $0, $0, $EE
 	dc.b $EE, $1, $0, $6, $6
 	dc.b $FE, $E, $0, $8, $F6
 	dc.b $FE, $0, $0, $14, $EE
-Map_6161_34D: 	dc.b $3
+Map_fc31_34D: 	dc.b $3
 	dc.b $EE, $9, $0, $0, $EE
 	dc.b $EE, $1, $0, $6, $6
 	dc.b $FE, $E, $0, $8, $F6
-Map_6161_35D: 	dc.b $2
+Map_fc31_35D: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EE
 	dc.b $F4, $B, $0, $6, $FE
-Map_6161_368: 	dc.b $2
+Map_fc31_368: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EE
 	dc.b $F4, $B, $0, $6, $FE
-Map_6161_373: 	dc.b $2
+Map_fc31_373: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EE
 	dc.b $F4, $B, $0, $6, $FE
-Map_6161_37E: 	dc.b $2
+Map_fc31_37E: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EE
 	dc.b $F4, $B, $0, $6, $FE
-Map_6161_389: 	dc.b $4
+Map_fc31_389: 	dc.b $4
 	dc.b $FA, $6, $0, $0, $EE
 	dc.b $F2, $4, $0, $6, $EE
 	dc.b $EA, $B, $0, $8, $FE
 	dc.b $A, $0, $0, $14, $FE
-Map_6161_39E: 	dc.b $2
+Map_fc31_39E: 	dc.b $2
 	dc.b $F2, $7, $0, $0, $EE
 	dc.b $EA, $B, $0, $8, $FE
-Map_6161_3A9: 	dc.b $4
+Map_fc31_3A9: 	dc.b $4
 	dc.b $FA, $6, $0, $0, $EE
 	dc.b $F2, $4, $0, $6, $EE
 	dc.b $EA, $B, $0, $8, $FE
 	dc.b $A, $0, $0, $14, $FE
-Map_6161_3BE: 	dc.b $2
+Map_fc31_3BE: 	dc.b $2
 	dc.b $F2, $7, $0, $0, $EE
 	dc.b $EA, $B, $0, $8, $FE
-Map_6161_3C9: 	dc.b $1
+Map_fc31_3C9: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_6161_3CF: 	dc.b $1
+Map_fc31_3CF: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_6161_3D5: 	dc.b $1
+Map_fc31_3D5: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_6161_3DB: 	dc.b $1
+Map_fc31_3DB: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_6161_3E1: 	dc.b $1
+Map_fc31_3E1: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_6161_3E7: 	dc.b $2
+Map_fc31_3E7: 	dc.b $2
 	dc.b $F4, $E, $0, $0, $EC
 	dc.b $F4, $2, $0, $C, $C
-Map_6161_3F2: 	dc.b $1
+Map_fc31_3F2: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_6161_3F8: 	dc.b $2
+Map_fc31_3F8: 	dc.b $2
 	dc.b $EC, $B, $0, $0, $F4
 	dc.b $C, $8, $0, $C, $F4
-Map_6161_403: 	dc.b $1
+Map_fc31_403: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_6161_409: 	dc.b $2
+Map_fc31_409: 	dc.b $2
 	dc.b $ED, $9, $0, $0, $F0
 	dc.b $FD, $E, $0, $6, $F0
-Map_6161_414: 	dc.b $2
+Map_fc31_414: 	dc.b $2
 	dc.b $ED, $9, $0, $0, $F0
 	dc.b $FD, $E, $0, $6, $F0
-Map_6161_41F: 	dc.b $4
+Map_fc31_41F: 	dc.b $4
 	dc.b $F4, $4, $0, $0, $FC
 	dc.b $FC, $D, $0, $2, $F4
 	dc.b $C, $8, $0, $A, $F4
 	dc.b $4, $0, $0, $D, $EC
-Map_6161_434: 	dc.b $3
+Map_fc31_434: 	dc.b $3
 	dc.b $EC, $8, $8, $0, $E8
 	dc.b $F4, $2, $8, $3, $0
 	dc.b $F4, $F, $8, $6, $E0
-Map_6161_444: 	dc.b $3
+Map_fc31_444: 	dc.b $3
 	dc.b $EC, $E, $8, $0, $E8
 	dc.b $4, $D, $8, $C, $E0
 	dc.b $C, $0, $18, $14, $0
-Map_6161_454: 	dc.b $3
+Map_fc31_454: 	dc.b $3
 	dc.b $F4, $D, $0, $0, $FC
 	dc.b $FC, $5, $0, $8, $EC
 	dc.b $4, $8, $0, $C, $FC
-Map_6161_464: 	dc.b $2
+Map_fc31_464: 	dc.b $2
 	dc.b $F4, $A, $0, $0, $E8
 	dc.b $F4, $A, $8, $0, $0
-Map_6161_46F: 	dc.b $3
+Map_fc31_46F: 	dc.b $3
 	dc.b $F4, $D, $0, $0, $E4
 	dc.b $FC, $0, $0, $8, $4
 	dc.b $4, $C, $0, $9, $EC
-Map_6161_47F: 	dc.b $3
+Map_fc31_47F: 	dc.b $3
 	dc.b $F8, $B, $D8, $0, $F0
 	dc.b $F0, $4, $D8, $C, $F0
 	dc.b $E8, $0, $D8, $E, $F8
-Map_6161_48F: 	dc.b $3
+Map_fc31_48F: 	dc.b $3
 	dc.b $F4, $D, $0, $0, $FC
 	dc.b $FC, $5, $0, $8, $EC
 	dc.b $4, $8, $0, $C, $FC
-Map_6161_49F: 	dc.b $4
+Map_fc31_49F: 	dc.b $4
 	dc.b $F8, $E, $0, $0, $E8
 	dc.b $0, $5, $0, $C, $8
 	dc.b $F8, $0, $0, $10, $8
 	dc.b $F0, $0, $0, $11, $F8
-Map_6161_4B4: 	dc.b $4
+Map_fc31_4B4: 	dc.b $4
 	dc.b $F8, $E, $0, $0, $E8
 	dc.b $0, $5, $0, $C, $8
 	dc.b $F8, $0, $0, $10, $8
 	dc.b $F0, $0, $0, $11, $F8
-Map_6161_4C9: 	dc.b $5
+Map_fc31_4C9: 	dc.b $5
 	dc.b $E8, $A, $0, $0, $F4
 	dc.b $F0, $1, $0, $9, $C
 	dc.b $0, $9, $0, $B, $F4
 	dc.b $10, $4, $0, $11, $F4
 	dc.b $0, $0, $0, $13, $EC
-Map_6161_4E3: 	dc.b $5
+Map_fc31_4E3: 	dc.b $5
 	dc.b $E8, $A, $0, $0, $F4
 	dc.b $E8, $1, $0, $9, $C
 	dc.b $0, $9, $0, $B, $F4
 	dc.b $10, $4, $0, $11, $F4
 	dc.b $0, $0, $0, $13, $EC
-Map_6161_4FD: 	dc.b $2
+Map_fc31_4FD: 	dc.b $2
 	dc.b $ED, $A, $0, $0, $F3
 	dc.b $5, $D, $0, $9, $EB
-Map_6161_508: 	dc.b $3
+Map_fc31_508: 	dc.b $3
 	dc.b $EC, $A, $0, $0, $F3
 	dc.b $4, $8, $0, $9, $F3
 	dc.b $C, $4, $0, $C, $F3
-Map_6161_518: 	dc.b $2
+Map_fc31_518: 	dc.b $2
 	dc.b $ED, $A, $0, $0, $F3
 	dc.b $5, $D, $0, $9, $EB
-Map_6161_523: 	dc.b $3
+Map_fc31_523: 	dc.b $3
 	dc.b $EC, $A, $0, $0, $F3
 	dc.b $4, $8, $0, $9, $F3
 	dc.b $C, $4, $0, $C, $F3
-Map_6161_533: 	dc.b $2
+Map_fc31_533: 	dc.b $2
 	dc.b $EC, $9, $0, $0, $F0
 	dc.b $FC, $E, $0, $6, $F0
-Map_6161_53E: 	dc.b $3
+Map_fc31_53E: 	dc.b $3
 	dc.b $EC, $A, $0, $0, $F0
 	dc.b $4, $5, $0, $9, $F8
 	dc.b $E4, $0, $0, $D, $F8
-Map_6161_54E: 	dc.b $3
+Map_fc31_54E: 	dc.b $3
 	dc.b $E8, $D, $0, $0, $EC
 	dc.b $E8, $1, $0, $8, $C
 	dc.b $F8, $B, $0, $A, $F4
-Map_6161_55E: 	dc.b $5
+Map_fc31_55E: 	dc.b $5
 	dc.b $E8, $D, $0, $0, $EC
 	dc.b $E8, $1, $0, $8, $C
 	dc.b $F8, $9, $0, $A, $F4
 	dc.b $8, $C, $0, $10, $F4
 	dc.b $10, $0, $0, $14, $F4
-Map_6161_578: 	dc.b $5
+Map_fc31_578: 	dc.b $5
 	dc.b $E8, $D, $0, $0, $EC
 	dc.b $E8, $1, $0, $8, $C
 	dc.b $F8, $9, $0, $A, $F4
 	dc.b $8, $C, $0, $10, $F4
 	dc.b $10, $0, $0, $14, $F4
-Map_6161_592: 	dc.b $2
+Map_fc31_592: 	dc.b $2
 	dc.b $EC, $8, $0, $0, $F0
 	dc.b $F4, $F, $0, $3, $F0
-Map_6161_59D: 	dc.b $3
+Map_fc31_59D: 	dc.b $3
 	dc.b $EC, $8, $0, $0, $F0
 	dc.b $F4, $E, $0, $3, $F0
 	dc.b $C, $8, $0, $F, $F8
-Map_6161_5AD: 	dc.b $1
+Map_fc31_5AD: 	dc.b $1
 	dc.b $F0, $B, $0, $0, $F4
-Map_6161_5B3: 	dc.b $1
+Map_fc31_5B3: 	dc.b $1
 	dc.b $F4, $6, $0, $0, $F8
-Map_6161_5B9: 	dc.b $1
+Map_fc31_5B9: 	dc.b $1
 	dc.b $F8, $1, $0, $0, $FC
-Map_6161_5BF: 	dc.b $3
+Map_fc31_5BF: 	dc.b $3
 	dc.b $F4, $D, $8, $0, $E4
 	dc.b $FC, $5, $8, $8, $4
 	dc.b $4, $8, $8, $C, $EC
-Map_6161_5CF: 	dc.b $3
+Map_fc31_5CF: 	dc.b $3
 	dc.b $F4, $D, $8, $0, $FC
 	dc.b $FC, $0, $8, $8, $F4
 	dc.b $4, $C, $8, $9, $F4
-Map_6161_5DF: 	dc.b $3
+Map_fc31_5DF: 	dc.b $3
 	dc.b $F0, $E, $0, $0, $EC
 	dc.b $F8, $1, $0, $C, $C
 	dc.b $8, $C, $0, $E, $F4
-Map_6161_5EF: 	dc.b $3
+Map_fc31_5EF: 	dc.b $3
 	dc.b $EB, $9, $0, $0, $F4
 	dc.b $FB, $E, $0, $6, $EC
 	dc.b $3, $1, $0, $12, $C
-Map_6161_5FF: 	dc.b $2
+Map_fc31_5FF: 	dc.b $2
 	dc.b $F0, $F, $0, $0, $EC
 	dc.b $F8, $2, $0, $10, $C
-Map_6161_60A: 	dc.b $3
+Map_fc31_60A: 	dc.b $3
 	dc.b $8, $D, $0, $0, $E8
 	dc.b $E8, $F, $0, $8, $F0
 	dc.b $8, $5, $0, $18, $8
