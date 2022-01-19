fb6b35e279d630bca58b8527b1b5bb4a326f4c49
GuesterReal <96834208+GuesterReal@users.noreply.github.com>
2022-01-19 10:56:11 +0300

    exe gone

--- _maps/Signpost.asm
+++ _maps/Signpost.asm
@@ -1,23 +1,23 @@
-Map_2b5d:
-Map_2b5d_0: 	dc.w Map_2b5d_A-Map_2b5d
-Map_2b5d_2: 	dc.w Map_2b5d_1A-Map_2b5d
-Map_2b5d_4: 	dc.w Map_2b5d_25-Map_2b5d
-Map_2b5d_6: 	dc.w Map_2b5d_30-Map_2b5d
-Map_2b5d_8: 	dc.w Map_2b5d_3B-Map_2b5d
-Map_2b5d_A: 	dc.b $3
+Map_64d2:
+Map_64d2_0: 	dc.w Map_64d2_A-Map_64d2
+Map_64d2_2: 	dc.w Map_64d2_1A-Map_64d2
+Map_64d2_4: 	dc.w Map_64d2_25-Map_64d2
+Map_64d2_6: 	dc.w Map_64d2_30-Map_64d2
+Map_64d2_8: 	dc.w Map_64d2_3B-Map_64d2
+Map_64d2_A: 	dc.b $3
 	dc.b $F0, $B, $0, $0, $E8
 	dc.b $F0, $B, $8, $0, $0
 	dc.b $10, $1, $0, $38, $FC
-Map_2b5d_1A: 	dc.b $2
+Map_64d2_1A: 	dc.b $2
 	dc.b $F0, $F, $0, $C, $F0
 	dc.b $10, $1, $0, $38, $FC
-Map_2b5d_25: 	dc.b $2
+Map_64d2_25: 	dc.b $2
 	dc.b $F0, $3, $0, $1C, $FC
 	dc.b $10, $1, $8, $38, $FC
-Map_2b5d_30: 	dc.b $2
+Map_64d2_30: 	dc.b $2
 	dc.b $F0, $F, $8, $C, $F0
 	dc.b $10, $1, $8, $38, $FC
-Map_2b5d_3B: 	dc.b $3
+Map_64d2_3B: 	dc.b $3
 	dc.b $F0, $B, $0, $20, $E8
 	dc.b $F0, $B, $0, $2C, $0
 	dc.b $10, $1, $0, $38, $FC
