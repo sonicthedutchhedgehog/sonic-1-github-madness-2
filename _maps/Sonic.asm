6d7864b2e9a8547156f2154c8b30ea13c46d7223
GuesterReal <96834208+GuesterReal@users.noreply.github.com>
2022-01-22 10:03:51 +0300

    i afrted

--- _maps/Sonic.asm
+++ _maps/Sonic.asm
@@ -1,438 +1,439 @@
-Map_edad:
-Map_edad_0: 	dc.w Map_edad_B2-Map_edad
-Map_edad_2: 	dc.w Map_edad_B3-Map_edad
-Map_edad_4: 	dc.w Map_edad_BE-Map_edad
-Map_edad_6: 	dc.w Map_edad_CE-Map_edad
-Map_edad_8: 	dc.w Map_edad_DE-Map_edad
-Map_edad_A: 	dc.w Map_edad_EE-Map_edad
-Map_edad_C: 	dc.w Map_edad_FE-Map_edad
-Map_edad_E: 	dc.w Map_edad_109-Map_edad
-Map_edad_10: 	dc.w Map_edad_114-Map_edad
-Map_edad_12: 	dc.w Map_edad_129-Map_edad
-Map_edad_14: 	dc.w Map_edad_139-Map_edad
-Map_edad_16: 	dc.w Map_edad_14E-Map_edad
-Map_edad_18: 	dc.w Map_edad_159-Map_edad
-Map_edad_1A: 	dc.w Map_edad_173-Map_edad
-Map_edad_1C: 	dc.w Map_edad_188-Map_edad
-Map_edad_1E: 	dc.w Map_edad_19D-Map_edad
-Map_edad_20: 	dc.w Map_edad_1B7-Map_edad
-Map_edad_22: 	dc.w Map_edad_1D6-Map_edad
-Map_edad_24: 	dc.w Map_edad_1F0-Map_edad
-Map_edad_26: 	dc.w Map_edad_205-Map_edad
-Map_edad_28: 	dc.w Map_edad_210-Map_edad
-Map_edad_2A: 	dc.w Map_edad_21B-Map_edad
-Map_edad_2C: 	dc.w Map_edad_230-Map_edad
-Map_edad_2E: 	dc.w Map_edad_23B-Map_edad
-Map_edad_30: 	dc.w Map_edad_24B-Map_edad
-Map_edad_32: 	dc.w Map_edad_26A-Map_edad
-Map_edad_34: 	dc.w Map_edad_289-Map_edad
-Map_edad_36: 	dc.w Map_edad_29E-Map_edad
-Map_edad_38: 	dc.w Map_edad_2B8-Map_edad
-Map_edad_3A: 	dc.w Map_edad_2CD-Map_edad
-Map_edad_3C: 	dc.w Map_edad_2E7-Map_edad
-Map_edad_3E: 	dc.w Map_edad_2F2-Map_edad
-Map_edad_40: 	dc.w Map_edad_2FD-Map_edad
-Map_edad_42: 	dc.w Map_edad_308-Map_edad
-Map_edad_44: 	dc.w Map_edad_313-Map_edad
-Map_edad_46: 	dc.w Map_edad_328-Map_edad
-Map_edad_48: 	dc.w Map_edad_338-Map_edad
-Map_edad_4A: 	dc.w Map_edad_34D-Map_edad
-Map_edad_4C: 	dc.w Map_edad_35D-Map_edad
-Map_edad_4E: 	dc.w Map_edad_368-Map_edad
-Map_edad_50: 	dc.w Map_edad_373-Map_edad
-Map_edad_52: 	dc.w Map_edad_37E-Map_edad
-Map_edad_54: 	dc.w Map_edad_389-Map_edad
-Map_edad_56: 	dc.w Map_edad_39E-Map_edad
-Map_edad_58: 	dc.w Map_edad_3A9-Map_edad
-Map_edad_5A: 	dc.w Map_edad_3BE-Map_edad
-Map_edad_5C: 	dc.w Map_edad_3C9-Map_edad
-Map_edad_5E: 	dc.w Map_edad_3CF-Map_edad
-Map_edad_60: 	dc.w Map_edad_3D5-Map_edad
-Map_edad_62: 	dc.w Map_edad_3DB-Map_edad
-Map_edad_64: 	dc.w Map_edad_3E1-Map_edad
-Map_edad_66: 	dc.w Map_edad_3E7-Map_edad
-Map_edad_68: 	dc.w Map_edad_3F2-Map_edad
-Map_edad_6A: 	dc.w Map_edad_3F8-Map_edad
-Map_edad_6C: 	dc.w Map_edad_403-Map_edad
-Map_edad_6E: 	dc.w Map_edad_409-Map_edad
-Map_edad_70: 	dc.w Map_edad_414-Map_edad
-Map_edad_72: 	dc.w Map_edad_41F-Map_edad
-Map_edad_74: 	dc.w Map_edad_434-Map_edad
-Map_edad_76: 	dc.w Map_edad_444-Map_edad
-Map_edad_78: 	dc.w Map_edad_454-Map_edad
-Map_edad_7A: 	dc.w Map_edad_464-Map_edad
-Map_edad_7C: 	dc.w Map_edad_46F-Map_edad
-Map_edad_7E: 	dc.w Map_edad_47F-Map_edad
-Map_edad_80: 	dc.w Map_edad_48F-Map_edad
-Map_edad_82: 	dc.w Map_edad_49F-Map_edad
-Map_edad_84: 	dc.w Map_edad_4B4-Map_edad
-Map_edad_86: 	dc.w Map_edad_4C9-Map_edad
-Map_edad_88: 	dc.w Map_edad_4E3-Map_edad
-Map_edad_8A: 	dc.w Map_edad_4FD-Map_edad
-Map_edad_8C: 	dc.w Map_edad_508-Map_edad
-Map_edad_8E: 	dc.w Map_edad_518-Map_edad
-Map_edad_90: 	dc.w Map_edad_523-Map_edad
-Map_edad_92: 	dc.w Map_edad_533-Map_edad
-Map_edad_94: 	dc.w Map_edad_53E-Map_edad
-Map_edad_96: 	dc.w Map_edad_54E-Map_edad
-Map_edad_98: 	dc.w Map_edad_55E-Map_edad
-Map_edad_9A: 	dc.w Map_edad_578-Map_edad
-Map_edad_9C: 	dc.w Map_edad_592-Map_edad
-Map_edad_9E: 	dc.w Map_edad_59D-Map_edad
-Map_edad_A0: 	dc.w Map_edad_5AD-Map_edad
-Map_edad_A2: 	dc.w Map_edad_5B3-Map_edad
-Map_edad_A4: 	dc.w Map_edad_5B9-Map_edad
-Map_edad_A6: 	dc.w Map_edad_5BF-Map_edad
-Map_edad_A8: 	dc.w Map_edad_5CF-Map_edad
-Map_edad_AA: 	dc.w Map_edad_5DF-Map_edad
-Map_edad_AC: 	dc.w Map_edad_5EF-Map_edad
-Map_edad_AE: 	dc.w Map_edad_5FF-Map_edad
-Map_edad_B0: 	dc.w Map_edad_60A-Map_edad
-Map_edad_B2: 	dc.b $0
-Map_edad_B3: 	dc.b $2
+Map_bab5:
+Map_bab5_0: 	dc.w Map_bab5_B2-Map_bab5
+Map_bab5_2: 	dc.w Map_bab5_B3-Map_bab5
+Map_bab5_4: 	dc.w Map_bab5_BE-Map_bab5
+Map_bab5_6: 	dc.w Map_bab5_CE-Map_bab5
+Map_bab5_8: 	dc.w Map_bab5_DE-Map_bab5
+Map_bab5_A: 	dc.w Map_bab5_EE-Map_bab5
+Map_bab5_C: 	dc.w Map_bab5_FE-Map_bab5
+Map_bab5_E: 	dc.w Map_bab5_109-Map_bab5
+Map_bab5_10: 	dc.w Map_bab5_114-Map_bab5
+Map_bab5_12: 	dc.w Map_bab5_129-Map_bab5
+Map_bab5_14: 	dc.w Map_bab5_139-Map_bab5
+Map_bab5_16: 	dc.w Map_bab5_14E-Map_bab5
+Map_bab5_18: 	dc.w Map_bab5_159-Map_bab5
+Map_bab5_1A: 	dc.w Map_bab5_173-Map_bab5
+Map_bab5_1C: 	dc.w Map_bab5_188-Map_bab5
+Map_bab5_1E: 	dc.w Map_bab5_19D-Map_bab5
+Map_bab5_20: 	dc.w Map_bab5_1B7-Map_bab5
+Map_bab5_22: 	dc.w Map_bab5_1D6-Map_bab5
+Map_bab5_24: 	dc.w Map_bab5_1F0-Map_bab5
+Map_bab5_26: 	dc.w Map_bab5_205-Map_bab5
+Map_bab5_28: 	dc.w Map_bab5_210-Map_bab5
+Map_bab5_2A: 	dc.w Map_bab5_21B-Map_bab5
+Map_bab5_2C: 	dc.w Map_bab5_230-Map_bab5
+Map_bab5_2E: 	dc.w Map_bab5_23B-Map_bab5
+Map_bab5_30: 	dc.w Map_bab5_24B-Map_bab5
+Map_bab5_32: 	dc.w Map_bab5_26A-Map_bab5
+Map_bab5_34: 	dc.w Map_bab5_289-Map_bab5
+Map_bab5_36: 	dc.w Map_bab5_29E-Map_bab5
+Map_bab5_38: 	dc.w Map_bab5_2B8-Map_bab5
+Map_bab5_3A: 	dc.w Map_bab5_2CD-Map_bab5
+Map_bab5_3C: 	dc.w Map_bab5_2E7-Map_bab5
+Map_bab5_3E: 	dc.w Map_bab5_2F2-Map_bab5
+Map_bab5_40: 	dc.w Map_bab5_2FD-Map_bab5
+Map_bab5_42: 	dc.w Map_bab5_308-Map_bab5
+Map_bab5_44: 	dc.w Map_bab5_313-Map_bab5
+Map_bab5_46: 	dc.w Map_bab5_328-Map_bab5
+Map_bab5_48: 	dc.w Map_bab5_338-Map_bab5
+Map_bab5_4A: 	dc.w Map_bab5_34D-Map_bab5
+Map_bab5_4C: 	dc.w Map_bab5_35D-Map_bab5
+Map_bab5_4E: 	dc.w Map_bab5_368-Map_bab5
+Map_bab5_50: 	dc.w Map_bab5_373-Map_bab5
+Map_bab5_52: 	dc.w Map_bab5_37E-Map_bab5
+Map_bab5_54: 	dc.w Map_bab5_389-Map_bab5
+Map_bab5_56: 	dc.w Map_bab5_39E-Map_bab5
+Map_bab5_58: 	dc.w Map_bab5_3A9-Map_bab5
+Map_bab5_5A: 	dc.w Map_bab5_3BE-Map_bab5
+Map_bab5_5C: 	dc.w Map_bab5_3C9-Map_bab5
+Map_bab5_5E: 	dc.w Map_bab5_3CF-Map_bab5
+Map_bab5_60: 	dc.w Map_bab5_3D5-Map_bab5
+Map_bab5_62: 	dc.w Map_bab5_3DB-Map_bab5
+Map_bab5_64: 	dc.w Map_bab5_3E1-Map_bab5
+Map_bab5_66: 	dc.w Map_bab5_3E7-Map_bab5
+Map_bab5_68: 	dc.w Map_bab5_3F2-Map_bab5
+Map_bab5_6A: 	dc.w Map_bab5_3F8-Map_bab5
+Map_bab5_6C: 	dc.w Map_bab5_403-Map_bab5
+Map_bab5_6E: 	dc.w Map_bab5_409-Map_bab5
+Map_bab5_70: 	dc.w Map_bab5_414-Map_bab5
+Map_bab5_72: 	dc.w Map_bab5_41F-Map_bab5
+Map_bab5_74: 	dc.w Map_bab5_434-Map_bab5
+Map_bab5_76: 	dc.w Map_bab5_444-Map_bab5
+Map_bab5_78: 	dc.w Map_bab5_454-Map_bab5
+Map_bab5_7A: 	dc.w Map_bab5_464-Map_bab5
+Map_bab5_7C: 	dc.w Map_bab5_46F-Map_bab5
+Map_bab5_7E: 	dc.w Map_bab5_47F-Map_bab5
+Map_bab5_80: 	dc.w Map_bab5_48F-Map_bab5
+Map_bab5_82: 	dc.w Map_bab5_49F-Map_bab5
+Map_bab5_84: 	dc.w Map_bab5_4B4-Map_bab5
+Map_bab5_86: 	dc.w Map_bab5_4C9-Map_bab5
+Map_bab5_88: 	dc.w Map_bab5_4E3-Map_bab5
+Map_bab5_8A: 	dc.w Map_bab5_4FD-Map_bab5
+Map_bab5_8C: 	dc.w Map_bab5_508-Map_bab5
+Map_bab5_8E: 	dc.w Map_bab5_518-Map_bab5
+Map_bab5_90: 	dc.w Map_bab5_523-Map_bab5
+Map_bab5_92: 	dc.w Map_bab5_533-Map_bab5
+Map_bab5_94: 	dc.w Map_bab5_53E-Map_bab5
+Map_bab5_96: 	dc.w Map_bab5_54E-Map_bab5
+Map_bab5_98: 	dc.w Map_bab5_55E-Map_bab5
+Map_bab5_9A: 	dc.w Map_bab5_578-Map_bab5
+Map_bab5_9C: 	dc.w Map_bab5_592-Map_bab5
+Map_bab5_9E: 	dc.w Map_bab5_59D-Map_bab5
+Map_bab5_A0: 	dc.w Map_bab5_5AD-Map_bab5
+Map_bab5_A2: 	dc.w Map_bab5_5B3-Map_bab5
+Map_bab5_A4: 	dc.w Map_bab5_5B9-Map_bab5
+Map_bab5_A6: 	dc.w Map_bab5_5BF-Map_bab5
+Map_bab5_A8: 	dc.w Map_bab5_5CF-Map_bab5
+Map_bab5_AA: 	dc.w Map_bab5_5DF-Map_bab5
+Map_bab5_AC: 	dc.w Map_bab5_5EF-Map_bab5
+Map_bab5_AE: 	dc.w Map_bab5_5FF-Map_bab5
+Map_bab5_B0: 	dc.w Map_bab5_60F-Map_bab5
+Map_bab5_B2: 	dc.b $0
+Map_bab5_B3: 	dc.b $2
 	dc.b $ED, $B, $0, $0, $F4
 	dc.b $D, $8, $0, $C, $F4
-Map_edad_BE: 	dc.b $3
+Map_bab5_BE: 	dc.b $3
 	dc.b $EC, $9, $0, $0, $F0
 	dc.b $FC, $9, $0, $6, $F0
 	dc.b $C, $8, $0, $C, $F8
-Map_edad_CE: 	dc.b $3
+Map_bab5_CE: 	dc.b $3
 	dc.b $EC, $9, $0, $0, $F0
 	dc.b $FC, $9, $0, $6, $F0
 	dc.b $C, $8, $0, $C, $F8
-Map_edad_DE: 	dc.b $3
+Map_bab5_DE: 	dc.b $3
 	dc.b $EC, $9, $0, $0, $F0
 	dc.b $FC, $9, $0, $6, $F0
 	dc.b $C, $8, $0, $C, $F8
-Map_edad_EE: 	dc.b $3
+Map_bab5_EE: 	dc.b $3
 	dc.b $EC, $A, $0, $0, $F0
 	dc.b $4, $8, $0, $9, $F0
 	dc.b $C, $8, $0, $C, $F8
-Map_edad_FE: 	dc.b $2
+Map_bab5_FE: 	dc.b $2
 	dc.b $EC, $D, $0, $0, $ED
 	dc.b $FC, $E, $0, $8, $F5
-Map_edad_109: 	dc.b $2
+Map_bab5_109: 	dc.b $2
 	dc.b $ED, $9, $0, $0, $F3
 	dc.b $FD, $A, $0, $6, $F3
-Map_edad_114: 	dc.b $4
+Map_bab5_114: 	dc.b $4
 	dc.b $EB, $D, $0, $0, $EC
 	dc.b $FB, $9, $0, $8, $EC
 	dc.b $FB, $6, $0, $E, $4
 	dc.b $B, $4, $0, $14, $EC
-Map_edad_129: 	dc.b $3
+Map_bab5_129: 	dc.b $3
 	dc.b $ED, $D, $0, $0, $EC
 	dc.b $FD, $C, $0, $8, $F4
 	dc.b $5, $9, $0, $C, $F4
-Map_edad_139: 	dc.b $4
+Map_bab5_139: 	dc.b $4
 	dc.b $EB, $9, $0, $0, $F4
 	dc.b $FB, $9, $0, $6, $EC
 	dc.b $FB, $6, $0, $C, $4
 	dc.b $B, $4, $0, $12, $EC
-Map_edad_14E: 	dc.b $2
+Map_bab5_14E: 	dc.b $2
 	dc.b $EC, $9, $0, $0, $F3
 	dc.b $FC, $E, $0, $6, $EB
-Map_edad_159: 	dc.b $5
+Map_bab5_159: 	dc.b $5
 	dc.b $EB, $9, $0, $0, $EB
 	dc.b $EB, $6, $0, $6, $3
 	dc.b $FB, $8, $0, $C, $EB
 	dc.b $3, $9, $0, $F, $F3
 	dc.b $13, $0, $0, $15, $FB
-Map_edad_173: 	dc.b $4
+Map_bab5_173: 	dc.b $4
 	dc.b $ED, $9, $0, $0, $ED
 	dc.b $ED, $1, $0, $6, $5
 	dc.b $FD, $D, $0, $8, $F5
 	dc.b $D, $8, $0, $10, $FD
-Map_edad_188: 	dc.b $4
+Map_bab5_188: 	dc.b $4
 	dc.b $EC, $9, $0, $0, $EC
 	dc.b $EC, $1, $0, $6, $4
 	dc.b $FC, $D, $0, $8, $F4
 	dc.b $C, $8, $0, $10, $FC
-Map_edad_19D: 	dc.b $5
+Map_bab5_19D: 	dc.b $5
 	dc.b $EB, $9, $0, $0, $EB
 	dc.b $EB, $5, $0, $6, $3
 	dc.b $FB, $D, $0, $A, $F3
 	dc.b $B, $8, $0, $12, $F3
 	dc.b $13, $4, $0, $15, $FB
-Map_edad_1B7: 	dc.b $6
+Map_bab5_1B7: 	dc.b $6
 	dc.b $EC, $9, $0, $0, $EC
 	dc.b $EC, $1, $0, $6, $4
 	dc.b $FC, $C, $0, $8, $EC
 	dc.b $4, $9, $0, $C, $F4
 	dc.b $FC, $5, $0, $12, $C
 	dc.b $F4, $0, $0, $16, $14
-Map_edad_1D6: 	dc.b $5
+Map_bab5_1D6: 	dc.b $5
 	dc.b $ED, $9, $0, $0, $ED
 	dc.b $ED, $1, $0, $6, $5
 	dc.b $FD, $0, $0, $8, $ED
 	dc.b $FD, $D, $0, $9, $F5
 	dc.b $D, $8, $0, $11, $FD
-Map_edad_1F0: 	dc.b $4
+Map_bab5_1F0: 	dc.b $4
 	dc.b $F4, $7, $0, $0, $EB
 	dc.b $EC, $9, $0, $8, $FB
 	dc.b $FC, $4, $0, $E, $FB
 	dc.b $4, $9, $0, $10, $FB
-Map_edad_205: 	dc.b $2
+Map_bab5_205: 	dc.b $2
 	dc.b $F4, $7, $0, $0, $EC
 	dc.b $EC, $B, $0, $8, $FC
-Map_edad_210: 	dc.b $2
+Map_bab5_210: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $ED
 	dc.b $F4, $A, $0, $6, $FD
-Map_edad_21B: 	dc.b $4
+Map_bab5_21B: 	dc.b $4
 	dc.b $F4, $6, $0, $0, $EB
 	dc.b $EC, $9, $0, $6, $FB
 	dc.b $FC, $4, $0, $C, $FB
 	dc.b $4, $9, $0, $E, $FB
-Map_edad_230: 	dc.b $2
+Map_bab5_230: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EC
 	dc.b $F4, $B, $0, $6, $FC
-Map_edad_23B: 	dc.b $3
+Map_bab5_23B: 	dc.b $3
 	dc.b $F4, $7, $0, $0, $ED
 	dc.b $EC, $0, $0, $8, $FD
 	dc.b $F4, $A, $0, $9, $FD
-Map_edad_24B: 	dc.b $6
+Map_bab5_24B: 	dc.b $6
 	dc.b $FD, $6, $0, $0, $EB
 	dc.b $ED, $4, $0, $6, $F3
 	dc.b $F5, $4, $0, $8, $EB
 	dc.b $F5, $A, $0, $A, $FB
 	dc.b $D, $0, $0, $13, $FB
 	dc.b $FD, $0, $0, $14, $13
-Map_edad_26A: 	dc.b $6
+Map_bab5_26A: 	dc.b $6
 	dc.b $FC, $6, $0, $0, $EC
 	dc.b $E4, $8, $0, $6, $F4
 	dc.b $EC, $4, $0, $9, $FC
 	dc.b $F4, $4, $0, $B, $EC
 	dc.b $F4, $A, $0, $D, $FC
 	dc.b $C, $0, $0, $16, $FC
-Map_edad_289: 	dc.b $4
+Map_bab5_289: 	dc.b $4
 	dc.b $FB, $6, $0, $0, $ED
 	dc.b $F3, $4, $0, $6, $ED
 	dc.b $EB, $A, $0, $8, $FD
 	dc.b $3, $4, $0, $11, $FD
-Map_edad_29E: 	dc.b $5
+Map_bab5_29E: 	dc.b $5
 	dc.b $FD, $6, $0, $0, $EB
 	dc.b $ED, $8, $0, $6, $F3
 	dc.b $F5, $4, $0, $9, $EB
 	dc.b $F5, $D, $0, $B, $FB
 	dc.b $5, $8, $0, $13, $FB
-Map_edad_2B8: 	dc.b $4
+Map_bab5_2B8: 	dc.b $4
 	dc.b $FC, $6, $0, $0, $EC
 	dc.b $F4, $4, $0, $6, $EC
 	dc.b $EC, $A, $0, $8, $FC
 	dc.b $4, $4, $0, $11, $FC
-Map_edad_2CD: 	dc.b $5
+Map_bab5_2CD: 	dc.b $5
 	dc.b $FB, $6, $0, $0, $ED
 	dc.b $EB, $A, $0, $6, $FD
 	dc.b $F3, $4, $0, $F, $ED
 	dc.b $3, $4, $0, $11, $FD
 	dc.b $B, $0, $0, $13, $FD
-Map_edad_2E7: 	dc.b $2
+Map_bab5_2E7: 	dc.b $2
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
-Map_edad_2F2: 	dc.b $2
+Map_bab5_2F2: 	dc.b $2
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
-Map_edad_2FD: 	dc.b $2
+Map_bab5_2FD: 	dc.b $2
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
-Map_edad_308: 	dc.b $2
+Map_bab5_308: 	dc.b $2
 	dc.b $EE, $9, $0, $0, $F4
 	dc.b $FE, $E, $0, $6, $EC
-Map_edad_313: 	dc.b $4
+Map_bab5_313: 	dc.b $4
 	dc.b $EE, $9, $0, $0, $EE
 	dc.b $EE, $1, $0, $6, $6
 	dc.b $FE, $E, $0, $8, $F6
 	dc.b $FE, $0, $0, $14, $EE
-Map_edad_328: 	dc.b $3
+Map_bab5_328: 	dc.b $3
 	dc.b $EE, $9, $0, $0, $EE
 	dc.b $EE, $1, $0, $6, $6
 	dc.b $FE, $E, $0, $8, $F6
-Map_edad_338: 	dc.b $4
+Map_bab5_338: 	dc.b $4
 	dc.b $EE, $9, $0, $0, $EE
 	dc.b $EE, $1, $0, $6, $6
 	dc.b $FE, $E, $0, $8, $F6
 	dc.b $FE, $0, $0, $14, $EE
-Map_edad_34D: 	dc.b $3
+Map_bab5_34D: 	dc.b $3
 	dc.b $EE, $9, $0, $0, $EE
 	dc.b $EE, $1, $0, $6, $6
 	dc.b $FE, $E, $0, $8, $F6
-Map_edad_35D: 	dc.b $2
+Map_bab5_35D: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EE
 	dc.b $F4, $B, $0, $6, $FE
-Map_edad_368: 	dc.b $2
+Map_bab5_368: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EE
 	dc.b $F4, $B, $0, $6, $FE
-Map_edad_373: 	dc.b $2
+Map_bab5_373: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EE
 	dc.b $F4, $B, $0, $6, $FE
-Map_edad_37E: 	dc.b $2
+Map_bab5_37E: 	dc.b $2
 	dc.b $F4, $6, $0, $0, $EE
 	dc.b $F4, $B, $0, $6, $FE
-Map_edad_389: 	dc.b $4
+Map_bab5_389: 	dc.b $4
 	dc.b $FA, $6, $0, $0, $EE
 	dc.b $F2, $4, $0, $6, $EE
 	dc.b $EA, $B, $0, $8, $FE
 	dc.b $A, $0, $0, $14, $FE
-Map_edad_39E: 	dc.b $2
+Map_bab5_39E: 	dc.b $2
 	dc.b $F2, $7, $0, $0, $EE
 	dc.b $EA, $B, $0, $8, $FE
-Map_edad_3A9: 	dc.b $4
+Map_bab5_3A9: 	dc.b $4
 	dc.b $FA, $6, $0, $0, $EE
 	dc.b $F2, $4, $0, $6, $EE
 	dc.b $EA, $B, $0, $8, $FE
 	dc.b $A, $0, $0, $14, $FE
-Map_edad_3BE: 	dc.b $2
+Map_bab5_3BE: 	dc.b $2
 	dc.b $F2, $7, $0, $0, $EE
 	dc.b $EA, $B, $0, $8, $FE
-Map_edad_3C9: 	dc.b $1
+Map_bab5_3C9: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_edad_3CF: 	dc.b $1
+Map_bab5_3CF: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_edad_3D5: 	dc.b $1
+Map_bab5_3D5: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_edad_3DB: 	dc.b $1
+Map_bab5_3DB: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_edad_3E1: 	dc.b $1
+Map_bab5_3E1: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_edad_3E7: 	dc.b $2
+Map_bab5_3E7: 	dc.b $2
 	dc.b $F4, $E, $0, $0, $EC
 	dc.b $F4, $2, $0, $C, $C
-Map_edad_3F2: 	dc.b $1
+Map_bab5_3F2: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_edad_3F8: 	dc.b $2
+Map_bab5_3F8: 	dc.b $2
 	dc.b $EC, $B, $0, $0, $F4
 	dc.b $C, $8, $0, $C, $F4
-Map_edad_403: 	dc.b $1
+Map_bab5_403: 	dc.b $1
 	dc.b $F0, $F, $0, $0, $F0
-Map_edad_409: 	dc.b $2
+Map_bab5_409: 	dc.b $2
 	dc.b $ED, $9, $0, $0, $F0
 	dc.b $FD, $E, $0, $6, $F0
-Map_edad_414: 	dc.b $2
+Map_bab5_414: 	dc.b $2
 	dc.b $ED, $9, $0, $0, $F0
 	dc.b $FD, $E, $0, $6, $F0
-Map_edad_41F: 	dc.b $4
+Map_bab5_41F: 	dc.b $4
 	dc.b $F4, $4, $0, $0, $FC
 	dc.b $FC, $D, $0, $2, $F4
 	dc.b $C, $8, $0, $A, $F4
 	dc.b $4, $0, $0, $D, $EC
-Map_edad_434: 	dc.b $3
+Map_bab5_434: 	dc.b $3
 	dc.b $EC, $8, $8, $0, $E8
 	dc.b $F4, $2, $8, $3, $0
 	dc.b $F4, $F, $8, $6, $E0
-Map_edad_444: 	dc.b $3
+Map_bab5_444: 	dc.b $3
 	dc.b $EC, $E, $8, $0, $E8
 	dc.b $4, $D, $8, $C, $E0
 	dc.b $C, $0, $18, $14, $0
-Map_edad_454: 	dc.b $3
+Map_bab5_454: 	dc.b $3
 	dc.b $F4, $D, $0, $0, $FC
 	dc.b $FC, $5, $0, $8, $EC
 	dc.b $4, $8, $0, $C, $FC
-Map_edad_464: 	dc.b $2
+Map_bab5_464: 	dc.b $2
 	dc.b $F4, $A, $0, $0, $E8
 	dc.b $F4, $A, $8, $0, $0
-Map_edad_46F: 	dc.b $3
+Map_bab5_46F: 	dc.b $3
 	dc.b $F4, $D, $0, $0, $E4
 	dc.b $FC, $0, $0, $8, $4
 	dc.b $4, $C, $0, $9, $EC
-Map_edad_47F: 	dc.b $3
+Map_bab5_47F: 	dc.b $3
 	dc.b $F8, $B, $D8, $0, $F0
 	dc.b $F0, $4, $D8, $C, $F0
 	dc.b $E8, $0, $D8, $E, $F8
-Map_edad_48F: 	dc.b $3
+Map_bab5_48F: 	dc.b $3
 	dc.b $F4, $D, $0, $0, $FC
 	dc.b $FC, $5, $0, $8, $EC
 	dc.b $4, $8, $0, $C, $FC
-Map_edad_49F: 	dc.b $4
+Map_bab5_49F: 	dc.b $4
 	dc.b $F8, $E, $0, $0, $E8
 	dc.b $0, $5, $0, $C, $8
 	dc.b $F8, $0, $0, $10, $8
 	dc.b $F0, $0, $0, $11, $F8
-Map_edad_4B4: 	dc.b $4
+Map_bab5_4B4: 	dc.b $4
 	dc.b $F8, $E, $0, $0, $E8
 	dc.b $0, $5, $0, $C, $8
 	dc.b $F8, $0, $0, $10, $8
 	dc.b $F0, $0, $0, $11, $F8
-Map_edad_4C9: 	dc.b $5
+Map_bab5_4C9: 	dc.b $5
 	dc.b $E8, $A, $0, $0, $F4
 	dc.b $F0, $1, $0, $9, $C
 	dc.b $0, $9, $0, $B, $F4
 	dc.b $10, $4, $0, $11, $F4
 	dc.b $0, $0, $0, $13, $EC
-Map_edad_4E3: 	dc.b $5
+Map_bab5_4E3: 	dc.b $5
 	dc.b $E8, $A, $0, $0, $F4
 	dc.b $E8, $1, $0, $9, $C
 	dc.b $0, $9, $0, $B, $F4
 	dc.b $10, $4, $0, $11, $F4
 	dc.b $0, $0, $0, $13, $EC
-Map_edad_4FD: 	dc.b $2
+Map_bab5_4FD: 	dc.b $2
 	dc.b $ED, $A, $0, $0, $F3
 	dc.b $5, $D, $0, $9, $EB
-Map_edad_508: 	dc.b $3
+Map_bab5_508: 	dc.b $3
 	dc.b $EC, $A, $0, $0, $F3
 	dc.b $4, $8, $0, $9, $F3
 	dc.b $C, $4, $0, $C, $F3
-Map_edad_518: 	dc.b $2
+Map_bab5_518: 	dc.b $2
 	dc.b $ED, $A, $0, $0, $F3
 	dc.b $5, $D, $0, $9, $EB
-Map_edad_523: 	dc.b $3
+Map_bab5_523: 	dc.b $3
 	dc.b $EC, $A, $0, $0, $F3
 	dc.b $4, $8, $0, $9, $F3
 	dc.b $C, $4, $0, $C, $F3
-Map_edad_533: 	dc.b $2
+Map_bab5_533: 	dc.b $2
 	dc.b $EC, $9, $0, $0, $F0
 	dc.b $FC, $E, $0, $6, $F0
-Map_edad_53E: 	dc.b $3
+Map_bab5_53E: 	dc.b $3
 	dc.b $EC, $A, $0, $0, $F0
 	dc.b $4, $5, $0, $9, $F8
 	dc.b $E4, $0, $0, $D, $F8
-Map_edad_54E: 	dc.b $3
+Map_bab5_54E: 	dc.b $3
 	dc.b $E8, $D, $0, $0, $EC
 	dc.b $E8, $1, $0, $8, $C
 	dc.b $F8, $B, $0, $A, $F4
-Map_edad_55E: 	dc.b $5
+Map_bab5_55E: 	dc.b $5
 	dc.b $E8, $D, $0, $0, $EC
 	dc.b $E8, $1, $0, $8, $C
 	dc.b $F8, $9, $0, $A, $F4
 	dc.b $8, $C, $0, $10, $F4
 	dc.b $10, $0, $0, $14, $F4
-Map_edad_578: 	dc.b $5
+Map_bab5_578: 	dc.b $5
 	dc.b $E8, $D, $0, $0, $EC
 	dc.b $E8, $1, $0, $8, $C
 	dc.b $F8, $9, $0, $A, $F4
 	dc.b $8, $C, $0, $10, $F4
 	dc.b $10, $0, $0, $14, $F4
-Map_edad_592: 	dc.b $2
+Map_bab5_592: 	dc.b $2
 	dc.b $EC, $8, $0, $0, $F0
 	dc.b $F4, $F, $0, $3, $F0
-Map_edad_59D: 	dc.b $3
+Map_bab5_59D: 	dc.b $3
 	dc.b $EC, $8, $0, $0, $F0
 	dc.b $F4, $E, $0, $3, $F0
 	dc.b $C, $8, $0, $F, $F8
-Map_edad_5AD: 	dc.b $1
+Map_bab5_5AD: 	dc.b $1
 	dc.b $F0, $B, $0, $0, $F4
-Map_edad_5B3: 	dc.b $1
+Map_bab5_5B3: 	dc.b $1
 	dc.b $F4, $6, $0, $0, $F8
-Map_edad_5B9: 	dc.b $1
+Map_bab5_5B9: 	dc.b $1
 	dc.b $F8, $1, $0, $0, $FC
-Map_edad_5BF: 	dc.b $3
+Map_bab5_5BF: 	dc.b $3
 	dc.b $F4, $D, $8, $0, $E4
 	dc.b $FC, $5, $8, $8, $4
 	dc.b $4, $8, $8, $C, $EC
-Map_edad_5CF: 	dc.b $3
+Map_bab5_5CF: 	dc.b $3
 	dc.b $F4, $D, $8, $0, $FC
 	dc.b $FC, $0, $8, $8, $F4
 	dc.b $4, $C, $8, $9, $F4
-Map_edad_5DF: 	dc.b $3
+Map_bab5_5DF: 	dc.b $3
 	dc.b $F0, $E, $0, $0, $EC
 	dc.b $F8, $1, $0, $C, $C
 	dc.b $8, $C, $0, $E, $F4
-Map_edad_5EF: 	dc.b $3
+Map_bab5_5EF: 	dc.b $3
 	dc.b $EB, $9, $0, $0, $F4
 	dc.b $FB, $E, $0, $6, $EC
 	dc.b $3, $1, $0, $12, $C
-Map_edad_5FF: 	dc.b $2
-	dc.b $F0, $F, $0, $0, $EC
-	dc.b $F8, $2, $0, $10, $C
-Map_edad_60A: 	dc.b $3
+Map_bab5_5FF: 	dc.b $3
+	dc.b $F0, $E, $0, $0, $EC
+	dc.b $F8, $1, $0, $C, $C
+	dc.b $8, $C, $0, $E, $F4
+Map_bab5_60F: 	dc.b $3
 	dc.b $8, $D, $0, $0, $E8
 	dc.b $E8, $F, $0, $8, $F0
 	dc.b $8, $5, $0, $18, $8
